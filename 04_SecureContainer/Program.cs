7798e29b4c14154669c7827f426aa1ac0c53f642
terence-john davies <t-j@terence-john.co.uk>
2019-12-06 19:28:34 +0000

    day 4 part 1 complete

--- 04_SecureContainer/Program.cs
+++ 04_SecureContainer/Program.cs
@@ -6,18 +6,49 @@ namespace _04_SecureContainer
     {
         static void Main()
         {
-            string[] theData = { "12", "14", "1969", "100756" };
+            string theData = "145852-616942";
 
-            Part1(theData);
+            int loVal = Int32.Parse(theData.Substring(0, 6));
+            int hiVal = Int32.Parse(theData.Substring(7, 6));
 
-            Part2(theData);
+            Part1(loVal, hiVal);
+
+            Part2(loVal, hiVal);
 
         }
-        static void Part1(string[] theData)
+        static void Part1(int loVal, int hiVal)
         {
+            int result = 0;
+
+            int i = loVal;
+            while (i <= hiVal)
+            {
+                if (Part1Check(i.ToString()))
+                {
+                    result++;
+                }
+                i++;
+            }
+            Console.WriteLine($"{result}");
         }
-        static void Part2(string[] theData)
+        static void Part2(int loVal, int hiVal)
         {
+            Console.WriteLine($"{loVal} - {hiVal}");
+        }
+        static bool Part1Check(string str)
+        {
+            bool asc = true;
+            bool dbl = false;
+
+            for ( int i = 1; i < str.Length && asc ; i++)
+            {
+                if (str[i - 1] > str[i])
+                    asc = false;
+                if (str[i - 1] == str[i])
+                    dbl = true;
+            }
+
+            return asc && dbl;
         }
     }
 }
