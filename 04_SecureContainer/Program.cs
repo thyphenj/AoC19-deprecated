f5fcd23c2ee4d7cb28270b1114c6f1bf42749ef8
terence-john davies <t-j@terence-john.co.uk>
2019-12-06 18:48:19 +0000

    start of day 4

---
+++ 04_SecureContainer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04_SecureContainer
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] theData = { "12", "14", "1969", "100756" };
+
+            Part1(theData);
+
+            Part2(theData);
+
+        }
+        static void Part1(string[] theData)
+        {
+        }
+        static void Part2(string[] theData)
+        {
+        }
+    }
+}
