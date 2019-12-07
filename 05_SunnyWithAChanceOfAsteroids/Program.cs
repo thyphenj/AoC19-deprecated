e009975b0df11dd9b4b81132da67e9475cf61ac7
terence-john davies <t-j@terence-john.co.uk>
2019-12-07 11:52:13 +0000

    created Day 5 part 1

---
+++ 05_SunnyWithAChanceOfAsteroids/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _05_SunnyWithAChanceOfAsteroids
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
