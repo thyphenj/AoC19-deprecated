a140f091a8c01313b2255f6b127474b71757bfec
terence-john davies <t-j@terence-john.co.uk>
2020-01-01 22:26:56 +0000

    overnight

---
+++ 07_AmplificationCircuit/IntCode.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07_AmplificationCircuit
+{
+    class IntCode
+    {
+        private int? Phase = null;
+
+        private int sp = 0;
+
+        List<int> CodeList = new List<int>();
+        int[] Code;
+
+        public IntCode(string codeString)
+        {
+            foreach (var n in codeString.Split(','))
+                CodeList.Add(int.Parse(n));
+            Code = CodeList.ToArray();
+        }
+
+        public void Run(int pha, Queue queue)
+        {
+            bool completed = false;
+            while (!completed)
+            {
+                int op = Code[sp] % 100;
+
+                bool mod1 = 1 == (Code[sp] / 100) % 10;
+                bool mod2 = 1 == (Code[sp] / 1000) % 10;
+                bool mod3 = 1 == (Code[sp] / 10000) % 10;
+
+                int x = mod1 ? sp + 1 : Code[sp + 1];
+                int y = mod2 ? sp + 2 : Code[sp + 2];
+                int z = mod3 ? sp + 3 : Code[sp + 3];
+
+                switch (op)
+                {
+                    case 1:     // add x,y => z
+
+                        Code[z] = Code[x] + Code[y];
+
+                        sp += 4;
+                        break;
+
+                    case 2:     // mul x,y => z
+
+                        Code[z] = Code[x] * Code[y];
+
+                        sp += 4;
+                        break;
+
+                    case 3:     // rea => x
+
+                        {
+                            int val;
+
+                            if (Phase.HasValue)
+                            {
+                                Phase = pha;
+                                val = pha;
+                            }
+                            else
+                            {
+                                val = queue.Read();
+                            }
+
+                            Code[x] = val;
+
+                            sp += 2;
+                            break;
+                        }
+                    case 4:     // wri x
+
+                        queue.Write(Code[x]);
+
+                        sp += 2;
+                        completed = true;
+
+                        break;
+
+                    case 5:     // jnz x => y
+
+                        if (Code[x] != 0)
+                            sp = Code[y];
+                        else
+                            sp += 3;
+
+                        break;
+
+                    case 6:     //jez x => y
+
+                        if (Code[x] == 0)
+                            sp = Code[y];
+                        else
+                            sp += 3;
+
+                        break;
+
+                    case 7:     // lt x,y => z
+
+                        Code[z] = (Code[x] < Code[y]) ? 1 : 0;
+
+                        sp += 4;
+
+                        break;
+
+                    case 8:     // eq x,y => z
+
+                        Code[z] = (Code[x] == Code[y]) ? 1 : 0;
+
+                        sp += 4;
+
+                        break;
+
+                    case 99:
+
+                        completed = true;
+
+                        return;
+
+                    default:
+                        Console.WriteLine($"*****************************BAD OPCODE {op} AT LOCATION {sp}");
+                        break;
+                }
+                Logit();
+            }
+            return;
+
+        }
+        private void Logit()
+        {
+            foreach (var x in Code)
+            {
+                Console.Write($"{x}, ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- 07_AmplificationCircuit/Program.cs
+++ 07_AmplificationCircuit/Program.cs
@@ -13,19 +13,21 @@ namespace _07_AmplificationCircuit
             string programText;
             int answer;
 
-            programText = File.ReadAllText("data.txt");
-            //programText = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0";
+            //programText = File.ReadAllText("data.txt");
+            programText = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0";
             //programText = "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0";
             //programText = "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0";
             //programText = "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5";
 
             answer = Part1(programText);
 
-            Console.WriteLine(answer);
             //answer = Part2(programText);
+
+            Console.WriteLine(answer);
 
         }
         //-------------------------------------------------------------------------------------------------------
+
         static int Part1(string programText)
         {
             int max = 0;
@@ -34,32 +36,24 @@ namespace _07_AmplificationCircuit
 
             foreach (var perm in perms)
             {
-                ProgramCode ampA = new ProgramCode("ampA", programText);
-                ProgramCode ampB = new ProgramCode("ampB", programText);
-                ProgramCode ampC = new ProgramCode("ampC", programText);
-                ProgramCode ampD = new ProgramCode("ampD", programText);
-                ProgramCode ampE = new ProgramCode("ampE", programText);
-
-                string phaseString = string.Join(" ", perm);
                 int[] phases = perm.ToArray();
 
-                int transValue = 0;
+                IntCode[] amps = new IntCode[5];
 
-                bool completed = false;
-                completed = ampA.Run(transValue, out transValue, phases[0]);
-                completed = ampB.Run(transValue, out transValue, phases[1]);
-                completed = ampC.Run(transValue, out transValue, phases[2]);
-                completed = ampD.Run(transValue, out transValue, phases[3]);
-                completed = ampE.Run(transValue, out transValue, phases[4]);
+                for (int i = 0; i < 5; i++)
+                    amps[i] = new IntCode(programText);
 
-                if (transValue > max)
+                var IO = new Queue(0);
+
+                for (int i = 0; i < 5; i++)
                 {
-                    max = transValue;
-                    Console.WriteLine($"{phaseString} - {transValue}");
+                    amps[i].Run(phases[i], IO);
                 }
+                Console.WriteLine();
             }
             return max;
         }
+
         //-------------------------------------------------------------------------------------------------------
         static int Part2(string programText)
         {
