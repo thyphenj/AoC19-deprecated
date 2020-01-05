e334b778ca2ef80e6b39a5eb96e52b4ae79cbf9b
terence-john davies <t-j@terence-john.co.uk>
2020-01-05 12:28:52 +0000

    started day 10

---
+++ 10_MonitoringStation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _10_MonitoringStation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
