2f2810ff7282a18fe479554bd465ab734ef70615
terence-john @work <t-j@terence-john.co.uk>
2020-01-06 17:23:27 +0000

    buggy overnight

--- 10_MonitoringStation/Program.cs
+++ 10_MonitoringStation/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace _10_MonitoringStation
 {
@@ -8,50 +9,105 @@ namespace _10_MonitoringStation
         public static char[] grid;
         public static int width;
         public static int height;
+        public static SortedSet<(int, int)> direction;
 
         static void Main()
         {
             string programText;
 
             programText = File.ReadAllText("data.txt");
+            //programText = ".#..#\n" + ".....\n" + "#####\n" + "....#\n" + "...##";
 
-            populateGrid(programText);
+            PopulateGrid(programText);
 
-            printGrid();
+            PrintGrid();
 
-            iterateGrid();
+            IterateGrid();
         }
 
-        private static void iterateGrid()
+        private static void IterateGrid()
         {
             int[] values = new int[grid.Length];
-
-            for (int pos = 0; pos < grid.Length; pos++)
-            {
-                if (grid[pos] == '.')
-                    values[pos] = 0;
-                else
-                {
-                    values[pos] = scanFrom(pos);
-                }
-            }
-
 
             for (int y = 0; y < height; y++)
             {
                 for (int x = 0; x < width; x++)
                 {
-                    Console.Write($"{values[loc(x, y)],2}");
+                    if (grid[Loc(x, y)] == '.')
+                        values[Loc(x, y)] = 0;
+                    else
+                        values[Loc(x, y)] = ScanFrom(x, y);
+                }
+            }
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Console.Write($"{values[Loc(x, y)],2}");
                 }
                 Console.WriteLine();
             }
         }
 
-        private static int scanFrom(int pos)
+        private static int ScanFrom(int x0, int y0)
         {
+            Console.WriteLine($"\n\n*****from {x0},{y0}");
+
+            direction = new SortedSet<(int, int)>();
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    int yDelta = y0 - y;
+                    int xDelta = x0 - x;
+                    if (xDelta != 0 || yDelta != 0)
+                    {
+                        (xDelta, yDelta) = HCF(xDelta, yDelta);
+
+                        direction.Add((xDelta, yDelta));
+                    }
+                }
+            }
+
+            foreach (var (x, y) in direction)
+            {
+                int factor = 1;
+                bool notFound = true;
+
+                while (notFound && x * factor < width && y * factor < width)
+                {
+                    if (grid[Loc(x * factor, y * factor)] == '#')
+                    {
+                        notFound = false;
+                        Console.WriteLine($"**hit at {x*factor},{y*factor}");
+                    }
+                }
+            }
+
             return 1;
         }
-        private static void populateGrid(string programText)
+        public static (int, int) HCF(int a, int b)
+        {
+            int x = a;
+            int y = b;
+
+            if (x == 0) { return (0, Math.Sign(y)); }
+            if (y == 0) { return (Math.Sign(x), 0); }
+
+            foreach (int n in new int[] { 2, 3, 5, 7, 11, 13, 17, 19, 21, 23, 29, 31 })
+            {
+                if (Math.Abs(x) >= n && Math.Abs(y) >= n)
+                    while (x % n == 0 && y % n == 0)
+                    {
+                        x /= n;
+                        y /= n;
+                    }
+            }
+            return (x, y);
+        }
+        private static void PopulateGrid(string programText)
         {
             width = programText.IndexOf('\n');
             height = programText.Length / width;
@@ -67,7 +123,7 @@ namespace _10_MonitoringStation
                 foreach (var ch in row)
                 {
 
-                    grid[loc(x, y)] = ch;
+                    grid[Loc(x, y)] = ch;
                     x++;
                 }
                 y++;
@@ -75,20 +131,20 @@ namespace _10_MonitoringStation
 
         }
 
-        public static void printGrid()
+        public static void PrintGrid()
         {
             // Print out the grid
             for (int y = 0; y < height; y++)
             {
                 for (int x = 0; x < width; x++)
                 {
-                    Console.Write(grid[loc(x, y)]);
+                    Console.Write(grid[Loc(x, y)]);
                 }
                 Console.WriteLine();
             }
         }
 
-        public static int loc(int x, int y)
+        public static int Loc(int x, int y)
         {
             return x + y * width;
         }
