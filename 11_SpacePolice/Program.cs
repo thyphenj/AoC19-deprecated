ca4e9b2256f19da2b17957a1c6c500834268c025
terence-john davies <t-j@terence-john.co.uk>
2020-01-29 21:04:17 +0000

    day 11 part 1 start AGAIN

---
+++ 11_SpacePolice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _11_SpacePolice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
